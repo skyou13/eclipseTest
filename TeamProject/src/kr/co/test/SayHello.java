84bcb8f6fe000f5784435508d2c50bdbd7c32b94
skyou13 <skyou13@naver.com>
2017-03-28 11:13:37 +0900

    원격저장소에서 직접 수정한것

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -2,7 +2,9 @@ package kr.co.test;
 
 public class SayHello {
 	public static void main(String[] args) {
-		System.out.println("원격 저장소 테스트를 위한 실습입니다.");		s					
+		System.out.println("원격 저장소 테스트를 위한 실습입니다.");
+		System.out.println(" master skdks.")
+		
 }
 
 }
