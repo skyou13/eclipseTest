749f432c2b8c25994a81166946e95f19bc63baef
ph <ph@naver.com>
2017-03-28 11:14:42 +0900

    원격 저장소 충돌 테스트를 위한 실습입니다.

--- TeamProject/src/kr/co/test/SayHello.java
+++ TeamProject/src/kr/co/test/SayHello.java
@@ -3,6 +3,7 @@ package kr.co.test;
 public class SayHello {
 	public static void main(String[] args) {
 		System.out.println("원격 저장소 테스트를 위한 실습입니다.");
+		System.out.println("충돌 테스트를 위한 실습입니다.");
 	}
 
 }
